5944c5e0396b4f482db5f87d359b9aebff9aad6c
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-04-30 12:30:42 +0200

    added get functions

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,10 +1,30 @@
-import { Injectable } from '@angular/core';
-import { Note } from '../interfaces/note.interface'
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+    trashNotes: Note[] = [];
+    normalNotes: Note[] = [];
+    firestore: Firestore = inject(Firestore);
+  
+    constructor() { }
+
+    getNotesRef(){
+      return collection(this.firestore, 'notes');
+    }
+
+    getTrashRef(){
+      return collection(this.firestore, 'trash');
+    }
+
+    getSingleDocRef(colId:string, docId:string){
+      return doc(collection(this.firestore, colId), docId);
+    }
+
 }
